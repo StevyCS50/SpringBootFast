42a4252e4114f51c94755a8fa1d9640b304c21b9
Vyacheslav Artamonov <v.artamonov@goodt.me>
2022-10-14 17:26:58 +0500

    init commit

---
+++ src/test/java/com/book/springboot/learning/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.springboot.learning.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
